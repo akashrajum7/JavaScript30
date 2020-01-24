7e41b37f934a48851753e488ed36e750a904e66c
Glitch (akash-javascript30) <none>
2020-01-24 12:26:12 +0000

    🍩🎈 Checkpoint ./11 - Custom Video Player/scripts.js:9940059/2971

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,11 +1,12 @@
 // Get elements
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+let isClicking = false;
 
 // Write functions
 function togglePlay() {
@@ -13,11 +14,15 @@ function togglePlay() {
   video[method]();
 }
 function toggleButton() {
-  const icon = this.paused ? '►' : '❚ ❚';
+  const icon = this.paused ? "►" : "❚ ❚";
   toggle.textContent = icon;
 }
-function skip(){
-  this.
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeChange() {
+  if()
+  video[this.name] = this.value;
 }
 
 // Hook event listeners
@@ -25,4 +30,10 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("play", toggleButton);
 video.addEventListener("pause", toggleButton);
 toggle.addEventListener("click", togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip));
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => {
+  range.addEventListener("change", handleRangeChange);
+  range.addEventListener("mousedown", () => isClicking = true);
+  range.addEventListener("mouseup", () => isClicking = false);
+  range.addEventListener("mouseout", ()=> isClicking = false);
+});
